baaf03c4aec495e0fd9f5de9d75f932289942fe0
vinaykhanna <khanna.vinay@hotmail.com>
2016-08-12 19:55:33 +0530

    commit

---
+++ AngularApp8/Assignment2/js/JavaScript.js
@@ -0,0 +1,20 @@
+﻿/// <reference path="D:\vinay code\VinayProjects\AngularApp8\Assignment2\lib/angular.min.js" />
+var EmployeeModule = angular.module("EmployeeModule", []).controller("EmployeeController", function ($scope) {
+    $scope.Employees = [
+    {
+        Name: "ANUJ KUMAR", DateOfBirth: new Date("23-Nov-1978"), Gender: "male", Salary: 6789.90
+    },
+    {
+        Name: "AMIT KUMAR", DateOfBirth: new Date("17-Sept-1978"), Gender: "male", Salary: 678990.90
+    },
+    {
+        Name: "SUMIT KUMAR", DateOfBirth: new Date("15-Jun-1978"), Gender: "male", Salary: 67089.90
+    },
+    {
+        Name: "SWATI", DateOfBirth: new Date("11-Jan-1992"), Gender: "female", Salary: 65757.90
+    }
+    ];
+});
+
+
+
